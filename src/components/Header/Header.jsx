49998d11fdd8363351d5568ba568407490dd0c33
Gabriel Corrêa <gcorrea@bocavox.com>
2023-09-26 20:05:13 -0300

    feature: add routes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">KickStarter Clone</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -13,14 +13,14 @@ export default function Header() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home</a>
+            <Link to="/" className="nav-link">Home</Link>
           </li>
         </ul>            
       </div>
 
-      <a href="/cart">
+      <Link to="/cart">
         <FontAwesomeIcon icon={faCartShopping} />
-      </a>
+      </Link>
     </nav>
   )
 }
--- src/pages/ProjectsPage/ViewProjectPage.jsx
+++ src/pages/ProjectsPage/ViewProjectPage.jsx
@@ -7,17 +7,17 @@ export default function ViewProjectPage () {
     <BasePageComponent>
       <div className="container">
         <h2>Camisa Social</h2>
-        <div class="card mb-3">
-  
-  <div class="card-body">
-    <h5 class="card-title">Camisa Social Branca</h5>
-    <p class="card-text">Tecido leve na cor branca, simples, de manga comprida, casual.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
 
-</div>
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Camisa Social Branca</h5>
+
+            <p class="card-text">Tecido leve na cor branca, simples, de manga comprida, casual.</p>
+
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
       </div>
-    
     </BasePageComponent> 
   )
 }
