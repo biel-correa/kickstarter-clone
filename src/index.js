7729d7064f8203b59b14de374fab952669d13f1e
Kayani Pereira <kayani.andrade23@gmail.com>
2023-09-26 19:27:54 -0300

    id

--- src/index.js
+++ src/index.js
@@ -7,21 +7,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import CartPage from './pages/CartPage/CartPage';
+import IdPage from './pages/ProjectsPage/IdPage';
+
 
 
 const router = createBrowserRouter([
   {
-    path: '/cart',
+    path: '/cart', 
     element: <CartPage />
+  },
+  {
+    path: '/projects/10',
+    element: <IdPage />
   }
-])
+    ]
+  )
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} >
+      <App />
+    </RouterProvider>
   </React.StrictMode>
+  
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/ProjectsPage/IdPage.jsx
@@ -0,0 +1,27 @@
+import BasePageComponent from "../../components/BasePageComponent/BasePageComponent";
+
+
+export default function IdPage () {
+  return (
+    
+    <BasePageComponent>
+      <div className="container">
+        <h2>Camisa Social</h2>
+        <div class="card mb-3">
+  
+  <div class="card-body">
+    <h5 class="card-title">Camisa Social Branca</h5>
+    <p class="card-text">Tecido leve na cor branca, simples, de manga comprida, casual.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+
+</div>
+      </div>
+    
+    </BasePageComponent> 
+  )
+}
+
+
+
+<img className="profile-photo" src="./Images/Profile_picture.jpeg" alt={"Carlie Anglemire"}/>
