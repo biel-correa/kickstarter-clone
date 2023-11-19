02d428ef8a1c070c1c0497f915544032704741b5
mizael <mizael.pardal@smartnx.com>
2023-11-19 18:40:35 -0300

    Reestruturando projeto

---
+++ frontend/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom';
+import CartPage from './pages/CartPage/CartPage';
+import ProjectPage from './pages/ProjectsPage/ProjectsPage';
+import ViewProjectPage from './pages/ViewProjectPage/ViewProjectPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/projects" replace />
+  }, {
+    path: '/cart',
+    element: <CartPage />
+  }, {
+    path: '/projects',
+    element: <ProjectPage />
+  }, {
+    path: '/projects/:id',
+    element: <ViewProjectPage />
+  }
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  
+);
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
