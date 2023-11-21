eddc8e7295ce8cb199da48320a081bff1c29d123
biel-correa <gabriel@correa.dev.br>
2023-11-20 21:49:39 -0300

    feature: enable user to remove item from cart

--- frontend/src/pages/CartPage/CartPage.jsx
+++ frontend/src/pages/CartPage/CartPage.jsx
@@ -4,10 +4,16 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 export default function CartPage() {
-  const [products, setProducts] = useState(JSON.parse(localStorage.getItem('cart')) || [])
+  const [project, setProjects] = useState(JSON.parse(localStorage.getItem('cart')) || [])
+
+  const removeProduct = (product) => {
+    const newProjects = project.filter((item) => item.id !== product.id);
+    setProjects(newProjects);
+    localStorage.setItem('cart', JSON.stringify(newProjects));
+  }
 
   const renderProducts = () => {
-    if (products.length === 0) {
+    if (project.length === 0) {
       return (
         <div className="alert alert-info">
           Seu carrinho está vazio!
@@ -17,12 +23,12 @@ export default function CartPage() {
 
     return (
       <ul className="list-group">
-        {products.map((product) => (
+        {project.map((product) => (
           <li className="list-group-item">
             <h5>{product.name}</h5>
             <div className="d-flex justify-content-between w-100">
               <h6>R$ {product.price}</h6>
-              <button className="btn btn-sm btn-danger">
+              <button className="btn btn-sm btn-danger" onClick={removeProduct(product)}>
                 <FontAwesomeIcon icon={faTrash} />
               </button>
             </div>
@@ -32,10 +38,9 @@ export default function CartPage() {
     )
   }
 
-  const getTotalPrice = () => products
+  const getTotalPrice = () => project
     .reduce((total, product) => total + product.price, 0)
     .toFixed(2);
-
 
   return (
     <BasePageComponent>
@@ -46,7 +51,7 @@ export default function CartPage() {
           <div className="card-body">
             <h5 className="card-title">Resumo</h5>
             <h6 className="card-subtitle mb-2 text-muted">Total: R$ {getTotalPrice()}</h6>
-            <button className="btn btn-sm btn-success" disabled={products.length <= 0}>Finalizar Pedido</button>
+            <button className="btn btn-sm btn-success" disabled={project.length <= 0}>Finalizar Pedido</button>
           </div>
         </div>
 
